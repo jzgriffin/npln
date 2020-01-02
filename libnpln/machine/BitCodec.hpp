60a0e415929bf76ece8d10e8a1d05e020eea163a
Jeremiah Griffin <nokurn@gmail.com>
2020-01-01 23:26:36 -0800

    Add machine::BitCodec structure
    BitCodec will be used to encode and decode integers too small for
    conventional hardware, such as 4-bit nibbles and 12-bit addresses,
    inside of larger integers.

---
+++ libnpln/machine/BitCodec.hpp
@@ -0,0 +1,51 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef LIBNPLN_MACHINE_BITCODEC_HPP
+#define LIBNPLN_MACHINE_BITCODEC_HPP
+
+#include <cstddef>
+#include <limits>
+
+namespace libnpln::machine {
+
+template<typename TContainer, std::size_t TBitWidth, std::size_t TBitOffset>
+struct BitCodec
+{
+    using Container = TContainer;
+
+    static constexpr auto decode(Container const x) noexcept
+    {
+        return static_cast<Container>((x & bit_mask) >> bit_offset);
+    }
+
+    static constexpr auto encode(Container const x) noexcept
+    {
+        return static_cast<Container>((x << bit_offset) & bit_mask);
+    }
+
+    static constexpr auto bit_width = TBitWidth;
+    static constexpr auto bit_offset = TBitOffset;
+    static_assert(bit_width + bit_offset <=
+        std::numeric_limits<Container>::digits);
+
+    static constexpr auto bit_mask =
+        static_cast<Container>((std::numeric_limits<Container>::max() >>
+            (std::numeric_limits<Container>::digits - bit_width)) <<
+            bit_offset);
+};
+
+}
+
+#endif
---
+++ libnpln/machine/BitCodec.test.cpp
@@ -0,0 +1,46 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <libnpln/machine/BitCodec.hpp>
+
+#include <catch2/catch.hpp>
+
+using namespace libnpln::machine;
+
+TEST_CASE("BitCodec produces the correct masks", "[machine][bitcodec]")
+{
+    REQUIRE(BitCodec<std::uint8_t, 4, 0>::bit_mask == 0b00001111);
+    REQUIRE(BitCodec<std::uint8_t, 4, 1>::bit_mask == 0b00011110);
+    REQUIRE(BitCodec<std::uint8_t, 4, 2>::bit_mask == 0b00111100);
+    REQUIRE(BitCodec<std::uint8_t, 4, 3>::bit_mask == 0b01111000);
+    REQUIRE(BitCodec<std::uint8_t, 4, 4>::bit_mask == 0b11110000);
+}
+
+TEST_CASE("BitCodec decodes into the lower bits", "[machine][bitcodec]")
+{
+    REQUIRE(BitCodec<std::uint8_t, 4, 0>::decode(0b00001001) == 0b1001);
+    REQUIRE(BitCodec<std::uint8_t, 4, 1>::decode(0b00010010) == 0b1001);
+    REQUIRE(BitCodec<std::uint8_t, 4, 2>::decode(0b00100100) == 0b1001);
+    REQUIRE(BitCodec<std::uint8_t, 4, 3>::decode(0b01001000) == 0b1001);
+    REQUIRE(BitCodec<std::uint8_t, 4, 4>::decode(0b10010000) == 0b1001);
+}
+
+TEST_CASE("BitCodec encodes into the offset bits", "[machine][bitcodec]")
+{
+    REQUIRE(BitCodec<std::uint8_t, 4, 0>::encode(0b1001) == 0b00001001);
+    REQUIRE(BitCodec<std::uint8_t, 4, 1>::encode(0b1001) == 0b00010010);
+    REQUIRE(BitCodec<std::uint8_t, 4, 2>::encode(0b1001) == 0b00100100);
+    REQUIRE(BitCodec<std::uint8_t, 4, 3>::encode(0b1001) == 0b01001000);
+    REQUIRE(BitCodec<std::uint8_t, 4, 4>::encode(0b1001) == 0b10010000);
+}
