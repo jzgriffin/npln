95af0ea8cdf1266b7434ab2ee34aa5f8a2929194
Jeremiah Griffin <nokurn@gmail.com>
2020-01-03 18:41:06 -0800

    Add machine::Operator enumeration
    Operator is an exhaustive list of all opcodes defined in the CHIP-8
    instruction set.

---
+++ libnpln/machine/Operator.hpp
@@ -0,0 +1,130 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef LIBNPLN_MACHINE_OPERATOR_HPP
+#define LIBNPLN_MACHINE_OPERATOR_HPP
+
+#include <libnpln/machine/DataUnits.hpp>
+
+#include <fmt/format.h>
+
+#include <stdexcept>
+#include <string_view>
+
+namespace libnpln::machine {
+
+enum class Operator : Word
+{
+    cls = 0x00E0,
+    ret = 0x00EE,
+    jmp_a = 0x1000,
+    call_a = 0x2000,
+    seq_v_b = 0x3000,
+    sne_v_b = 0x4000,
+    seq_v_v = 0x5000,
+    mov_v_b = 0x6000,
+    add_v_b = 0x7000,
+    mov_v_v = 0x8000,
+    or_v_v = 0x8001,
+    and_v_v = 0x8002,
+    xor_v_v = 0x8003,
+    add_v_v = 0x8004,
+    sub_v_v = 0x8005,
+    shr_v = 0x8006,
+    subn_v_v = 0x8007,
+    shl_v = 0x800E,
+    sne_v_v = 0x9000,
+    mov_i_a = 0xA000,
+    jmp_v0_a = 0xB000,
+    rnd_v_b = 0xC000,
+    drw_v_v_n = 0xD000,
+    skp_v = 0xE09E,
+    sknp_v = 0xE0A1,
+    mov_v_dt = 0xF007,
+    wkp_v = 0xF00A,
+    mov_dt_v = 0xF015,
+    mov_st_v = 0xF018,
+    add_i_v = 0xF01E,
+    font_v = 0xF029,
+    bcd_v = 0xF033,
+    mov_ii_v = 0xF055,
+    mov_v_ii = 0xF065,
+};
+
+constexpr auto get_format_string(Operator const op) -> std::string_view
+{
+    switch (op) {
+        case Operator::cls: return "CLS";
+        case Operator::ret: return "RET";
+        case Operator::jmp_a: return "JMP {address}";
+        case Operator::call_a: return "CALL {address}";
+        case Operator::seq_v_b: return "SEQ %{Vx}, ${byte}";
+        case Operator::sne_v_b: return "SNE %{Vx}, ${byte}";
+        case Operator::seq_v_v: return "SEQ %{Vx}, %{Vy}";
+        case Operator::mov_v_b: return "MOV ${byte}, %{Vx}";
+        case Operator::add_v_b: return "ADD ${byte}, %{Vx}";
+        case Operator::mov_v_v: return "MOV %{Vy}, %{Vx}";
+        case Operator::or_v_v: return "OR %{Vy}, %{Vx}";
+        case Operator::and_v_v: return "AND %{Vy}, %{Vx}";
+        case Operator::xor_v_v: return "XOR %{Vy}, %{Vx}";
+        case Operator::add_v_v: return "ADD %{Vy}, %{Vx}";
+        case Operator::sub_v_v: return "SUB %{Vy}, %{Vx}";
+        case Operator::shr_v: return "SHR %{Vx}";
+        case Operator::subn_v_v: return "SUBN %{Vy}, %{Vx}";
+        case Operator::shl_v: return "SHL %{Vx}";
+        case Operator::sne_v_v: return "SNE %{Vx}, %{Vy}";
+        case Operator::mov_i_a: return "MOV {address}, %I";
+        case Operator::jmp_v0_a: return "JMP %V0({address})";
+        case Operator::rnd_v_b: return "RND ${byte}, %{Vx}";
+        case Operator::drw_v_v_n: return "DRW %{Vx}, %{Vy}, ${nibble}";
+        case Operator::skp_v: return "SKP %{Vx}";
+        case Operator::sknp_v: return "SKNP %{Vx}";
+        case Operator::mov_v_dt: return "MOV %DT, %{Vx}";
+        case Operator::wkp_v: return "WKP %{Vx}";
+        case Operator::mov_dt_v: return "MOV %{Vx}, %DT";
+        case Operator::mov_st_v: return "MOV %{Vx}, %ST";
+        case Operator::add_i_v: return "ADD %{Vx}, %I";
+        case Operator::font_v: return "FONT %{Vx}";
+        case Operator::bcd_v: return "BCD %{Vx}";
+        case Operator::mov_ii_v: return "MOV %V0..%{Vx}, (%I)";
+        case Operator::mov_v_ii: return "MOV (%I), %V0..%{Vx}";
+        default:
+            throw std::logic_error{"Missing Operator in get_format_string"};
+    }
+}
+
+}
+
+template<>
+struct fmt::formatter<libnpln::machine::Operator>
+{
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& context)
+    {
+        return context.begin();
+    }
+
+    // The string output by this function is itself a format string.  The
+    // format string may be formatted using named arguments whose names match
+    // the parameters in the operator's mnemonic.
+    template<typename FormatContext>
+    auto format(libnpln::machine::Operator const& value,
+        FormatContext& context)
+    {
+        return format_to(context.out(), "{}",
+            libnpln::machine::get_format_string(value));
+    }
+};
+
+#endif
---
+++ libnpln/machine/Operator.test.cpp
@@ -0,0 +1,171 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <libnpln/detail/cpp2b.hpp>
+#include <libnpln/machine/Operator.hpp>
+
+#include <catch2/catch.hpp>
+
+using namespace libnpln;
+using namespace libnpln::machine;
+
+TEST_CASE("Operators meet CHIP-8 specifications", "[machine][operator]")
+{
+    REQUIRE(detail::to_underlying(Operator::cls) == 0x00E0);
+    REQUIRE(detail::to_underlying(Operator::ret) == 0x00EE);
+    REQUIRE(detail::to_underlying(Operator::jmp_a) == 0x1000);
+    REQUIRE(detail::to_underlying(Operator::call_a) == 0x2000);
+    REQUIRE(detail::to_underlying(Operator::seq_v_b) == 0x3000);
+    REQUIRE(detail::to_underlying(Operator::sne_v_b) == 0x4000);
+    REQUIRE(detail::to_underlying(Operator::seq_v_v) == 0x5000);
+    REQUIRE(detail::to_underlying(Operator::mov_v_b) == 0x6000);
+    REQUIRE(detail::to_underlying(Operator::add_v_b) == 0x7000);
+    REQUIRE(detail::to_underlying(Operator::mov_v_v) == 0x8000);
+    REQUIRE(detail::to_underlying(Operator::or_v_v) == 0x8001);
+    REQUIRE(detail::to_underlying(Operator::and_v_v) == 0x8002);
+    REQUIRE(detail::to_underlying(Operator::xor_v_v) == 0x8003);
+    REQUIRE(detail::to_underlying(Operator::add_v_v) == 0x8004);
+    REQUIRE(detail::to_underlying(Operator::sub_v_v) == 0x8005);
+    REQUIRE(detail::to_underlying(Operator::shr_v) == 0x8006);
+    REQUIRE(detail::to_underlying(Operator::subn_v_v) == 0x8007);
+    REQUIRE(detail::to_underlying(Operator::shl_v) == 0x800E);
+    REQUIRE(detail::to_underlying(Operator::sne_v_v) == 0x9000);
+    REQUIRE(detail::to_underlying(Operator::mov_i_a) == 0xA000);
+    REQUIRE(detail::to_underlying(Operator::jmp_v0_a) == 0xB000);
+    REQUIRE(detail::to_underlying(Operator::rnd_v_b) == 0xC000);
+    REQUIRE(detail::to_underlying(Operator::drw_v_v_n) == 0xD000);
+    REQUIRE(detail::to_underlying(Operator::skp_v) == 0xE09E);
+    REQUIRE(detail::to_underlying(Operator::sknp_v) == 0xE0A1);
+    REQUIRE(detail::to_underlying(Operator::mov_v_dt) == 0xF007);
+    REQUIRE(detail::to_underlying(Operator::wkp_v) == 0xF00A);
+    REQUIRE(detail::to_underlying(Operator::mov_dt_v) == 0xF015);
+    REQUIRE(detail::to_underlying(Operator::mov_st_v) == 0xF018);
+    REQUIRE(detail::to_underlying(Operator::add_i_v) == 0xF01E);
+    REQUIRE(detail::to_underlying(Operator::font_v) == 0xF029);
+    REQUIRE(detail::to_underlying(Operator::bcd_v) == 0xF033);
+    REQUIRE(detail::to_underlying(Operator::mov_ii_v) == 0xF055);
+    REQUIRE(detail::to_underlying(Operator::mov_v_ii) == 0xF065);
+}
+
+TEST_CASE("Operators define format strings", "[machine][operator]")
+{
+    REQUIRE(get_format_string(Operator::cls) == "CLS");
+    REQUIRE(get_format_string(Operator::ret) == "RET");
+    REQUIRE(get_format_string(Operator::jmp_a) == "JMP {address}");
+    REQUIRE(get_format_string(Operator::call_a) == "CALL {address}");
+    REQUIRE(get_format_string(Operator::seq_v_b) == "SEQ %{Vx}, ${byte}");
+    REQUIRE(get_format_string(Operator::sne_v_b) == "SNE %{Vx}, ${byte}");
+    REQUIRE(get_format_string(Operator::seq_v_v) == "SEQ %{Vx}, %{Vy}");
+    REQUIRE(get_format_string(Operator::mov_v_b) == "MOV ${byte}, %{Vx}");
+    REQUIRE(get_format_string(Operator::add_v_b) == "ADD ${byte}, %{Vx}");
+    REQUIRE(get_format_string(Operator::mov_v_v) == "MOV %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::or_v_v) == "OR %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::and_v_v) == "AND %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::xor_v_v) == "XOR %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::add_v_v) == "ADD %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::sub_v_v) == "SUB %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::shr_v) == "SHR %{Vx}");
+    REQUIRE(get_format_string(Operator::subn_v_v) == "SUBN %{Vy}, %{Vx}");
+    REQUIRE(get_format_string(Operator::shl_v) == "SHL %{Vx}");
+    REQUIRE(get_format_string(Operator::sne_v_v) == "SNE %{Vx}, %{Vy}");
+    REQUIRE(get_format_string(Operator::mov_i_a) == "MOV {address}, %I");
+    REQUIRE(get_format_string(Operator::jmp_v0_a) == "JMP %V0({address})");
+    REQUIRE(get_format_string(Operator::rnd_v_b) == "RND ${byte}, %{Vx}");
+    REQUIRE(get_format_string(Operator::drw_v_v_n) ==
+        "DRW %{Vx}, %{Vy}, ${nibble}");
+    REQUIRE(get_format_string(Operator::skp_v) == "SKP %{Vx}");
+    REQUIRE(get_format_string(Operator::sknp_v) == "SKNP %{Vx}");
+    REQUIRE(get_format_string(Operator::mov_v_dt) == "MOV %DT, %{Vx}");
+    REQUIRE(get_format_string(Operator::wkp_v) == "WKP %{Vx}");
+    REQUIRE(get_format_string(Operator::mov_dt_v) == "MOV %{Vx}, %DT");
+    REQUIRE(get_format_string(Operator::mov_st_v) == "MOV %{Vx}, %ST");
+    REQUIRE(get_format_string(Operator::add_i_v) == "ADD %{Vx}, %I");
+    REQUIRE(get_format_string(Operator::font_v) == "FONT %{Vx}");
+    REQUIRE(get_format_string(Operator::bcd_v) == "BCD %{Vx}");
+    REQUIRE(get_format_string(Operator::mov_ii_v) == "MOV %V0..%{Vx}, (%I)");
+    REQUIRE(get_format_string(Operator::mov_v_ii) == "MOV (%I), %V0..%{Vx}");
+}
+
+TEST_CASE("Operators return a format string when formatted",
+    "[machine][operator]")
+{
+    REQUIRE(fmt::format("{}", Operator::cls) ==
+        get_format_string(Operator::cls));
+    REQUIRE(fmt::format("{}", Operator::ret) ==
+        get_format_string(Operator::ret));
+    REQUIRE(fmt::format("{}", Operator::jmp_a) ==
+        get_format_string(Operator::jmp_a));
+    REQUIRE(fmt::format("{}", Operator::call_a) ==
+        get_format_string(Operator::call_a));
+    REQUIRE(fmt::format("{}", Operator::seq_v_b) ==
+        get_format_string(Operator::seq_v_b));
+    REQUIRE(fmt::format("{}", Operator::sne_v_b) ==
+        get_format_string(Operator::sne_v_b));
+    REQUIRE(fmt::format("{}", Operator::seq_v_v) ==
+        get_format_string(Operator::seq_v_v));
+    REQUIRE(fmt::format("{}", Operator::mov_v_b) ==
+        get_format_string(Operator::mov_v_b));
+    REQUIRE(fmt::format("{}", Operator::add_v_b) ==
+        get_format_string(Operator::add_v_b));
+    REQUIRE(fmt::format("{}", Operator::mov_v_v) ==
+        get_format_string(Operator::mov_v_v));
+    REQUIRE(fmt::format("{}", Operator::or_v_v) ==
+        get_format_string(Operator::or_v_v));
+    REQUIRE(fmt::format("{}", Operator::and_v_v) ==
+        get_format_string(Operator::and_v_v));
+    REQUIRE(fmt::format("{}", Operator::xor_v_v) ==
+        get_format_string(Operator::xor_v_v));
+    REQUIRE(fmt::format("{}", Operator::add_v_v) ==
+        get_format_string(Operator::add_v_v));
+    REQUIRE(fmt::format("{}", Operator::sub_v_v) ==
+        get_format_string(Operator::sub_v_v));
+    REQUIRE(fmt::format("{}", Operator::shr_v) ==
+        get_format_string(Operator::shr_v));
+    REQUIRE(fmt::format("{}", Operator::subn_v_v) ==
+        get_format_string(Operator::subn_v_v));
+    REQUIRE(fmt::format("{}", Operator::shl_v) ==
+        get_format_string(Operator::shl_v));
+    REQUIRE(fmt::format("{}", Operator::sne_v_v) ==
+        get_format_string(Operator::sne_v_v));
+    REQUIRE(fmt::format("{}", Operator::mov_i_a) ==
+        get_format_string(Operator::mov_i_a));
+    REQUIRE(fmt::format("{}", Operator::jmp_v0_a) ==
+        get_format_string(Operator::jmp_v0_a));
+    REQUIRE(fmt::format("{}", Operator::rnd_v_b) ==
+        get_format_string(Operator::rnd_v_b));
+    REQUIRE(fmt::format("{}", Operator::drw_v_v_n) ==
+        get_format_string(Operator::drw_v_v_n));
+    REQUIRE(fmt::format("{}", Operator::skp_v) ==
+        get_format_string(Operator::skp_v));
+    REQUIRE(fmt::format("{}", Operator::sknp_v) ==
+        get_format_string(Operator::sknp_v));
+    REQUIRE(fmt::format("{}", Operator::mov_v_dt) ==
+        get_format_string(Operator::mov_v_dt));
+    REQUIRE(fmt::format("{}", Operator::wkp_v) ==
+        get_format_string(Operator::wkp_v));
+    REQUIRE(fmt::format("{}", Operator::mov_dt_v) ==
+        get_format_string(Operator::mov_dt_v));
+    REQUIRE(fmt::format("{}", Operator::mov_st_v) ==
+        get_format_string(Operator::mov_st_v));
+    REQUIRE(fmt::format("{}", Operator::add_i_v) ==
+        get_format_string(Operator::add_i_v));
+    REQUIRE(fmt::format("{}", Operator::font_v) ==
+        get_format_string(Operator::font_v));
+    REQUIRE(fmt::format("{}", Operator::bcd_v) ==
+        get_format_string(Operator::bcd_v));
+    REQUIRE(fmt::format("{}", Operator::mov_ii_v) ==
+        get_format_string(Operator::mov_ii_v));
+    REQUIRE(fmt::format("{}", Operator::mov_v_ii) ==
+        get_format_string(Operator::mov_v_ii));
+}
