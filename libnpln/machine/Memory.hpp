aa395491cf55caf6fab0d66690049278ba1f1f2c
Jeremiah Griffin <nokurn@gmail.com>
2020-01-11 22:51:37 -0800

    Add machine::Memory type

---
+++ libnpln/machine/Memory.hpp
@@ -0,0 +1,28 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef LIBNPLN_MACHINE_MEMORY_HPP
+#define LIBNPLN_MACHINE_MEMORY_HPP
+
+#include <libnpln/machine/DataUnits.hpp>
+
+#include <array>
+
+namespace libnpln::machine {
+
+using Memory = std::array<Byte, 0x1000>;
+
+}
+
+#endif
---
+++ libnpln/machine/Memory.test.cpp
@@ -0,0 +1,27 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <libnpln/machine/Memory.hpp>
+
+#include <catch2/catch.hpp>
+
+#include <type_traits>
+
+using namespace libnpln::machine;
+
+TEST_CASE("Memory stores 4096 bytes", "[machine][memory]")
+{
+    static_assert(std::is_same_v<Memory::value_type, Byte>);
+    REQUIRE(Memory{}.max_size() == 4096);
+}
