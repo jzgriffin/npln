7102a03e0f2fa450570c7a1ecd26c2d708df092d
Jeremiah Griffin <nokurn@gmail.com>
2020-01-12 00:28:50 -0800

    Add detail::static_cast_set function
    The static_cast_set function casts a std::set of one element type to
    another by applying static_cast to each element in the set.

---
+++ libnpln/detail/ContainerCast.hpp
@@ -0,0 +1,35 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef LIBNPLN_DETAIL_CONTAINERCAST_HPP
+#define LIBNPLN_DETAIL_CONTAINERCAST_HPP
+
+#include <set>
+
+namespace libnpln::detail {
+
+template<typename TTo, typename TFrom>
+auto static_cast_set(std::set<TFrom> const& sf) noexcept
+{
+    auto st = std::set<TTo>{};
+    for (auto&& x : sf) {
+        st.insert(static_cast<TTo>(x));
+    }
+
+    return st;
+}
+
+}
+
+#endif
