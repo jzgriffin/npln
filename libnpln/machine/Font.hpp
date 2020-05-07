44e00f61dfa01d611bbf8b8db0a0520a008d2040
Jeremiah Griffin <nokurn@gmail.com>
2020-05-06 17:25:18 -0700

    Add machine::Glyph type and font glyph set

---
+++ libnpln/machine/Font.hpp
@@ -0,0 +1,160 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef LIBNPLN_MACHINE_FONT_HPP
+#define LIBNPLN_MACHINE_FONT_HPP
+
+#include <libnpln/machine/DataUnits.hpp>
+#include <libnpln/machine/Memory.hpp>
+
+#include <array>
+#include <optional>
+
+namespace libnpln::machine {
+
+// Each font glyph is 8x5 pixels
+using Glyph = std::array<Byte, 5>;
+
+constexpr std::size_t glyph_size = Glyph{}.size();
+
+constexpr std::array<Glyph, 0x10> font_glyphs = {
+    Glyph{ // 0
+        0b1111'0000,
+        0b1001'0000,
+        0b1001'0000,
+        0b1001'0000,
+        0b1111'0000,
+    },
+    Glyph{ // 1
+        0b0010'0000,
+        0b0110'0000,
+        0b0010'0000,
+        0b0010'0000,
+        0b0111'0000,
+    },
+    Glyph{ // 2
+        0b1111'0000,
+        0b0001'0000,
+        0b1111'0000,
+        0b1000'0000,
+        0b1111'0000,
+    },
+    Glyph{ // 3
+        0b1111'0000,
+        0b0001'0000,
+        0b1111'0000,
+        0b0001'0000,
+        0b1111'0000,
+    },
+    Glyph{ // 4
+        0b1001'0000,
+        0b1001'0000,
+        0b1111'0000,
+        0b0001'0000,
+        0b0001'0000,
+    },
+    Glyph{ // 5
+        0b1111'0000,
+        0b1000'0000,
+        0b1111'0000,
+        0b0001'0000,
+        0b1111'0000,
+    },
+    Glyph{ // 6
+        0b1111'0000,
+        0b1000'0000,
+        0b1111'0000,
+        0b1001'0000,
+        0b1111'0000,
+    },
+    Glyph{ // 7
+        0b1111'0000,
+        0b0001'0000,
+        0b0010'0000,
+        0b0100'0000,
+        0b0100'0000,
+    },
+    Glyph{ // 8
+        0b1111'0000,
+        0b1001'0000,
+        0b1111'0000,
+        0b1001'0000,
+        0b1111'0000,
+    },
+    Glyph{ // 9
+        0b1111'0000,
+        0b1001'0000,
+        0b1111'0000,
+        0b0001'0000,
+        0b1111'0000,
+    },
+    Glyph{ // A
+        0b1111'0000,
+        0b1001'0000,
+        0b1111'0000,
+        0b1001'0000,
+        0b1001'0000,
+    },
+    Glyph{ // B
+        0b1110'0000,
+        0b1001'0000,
+        0b1110'0000,
+        0b1001'0000,
+        0b1110'0000,
+    },
+    Glyph{ // C
+        0b1111'0000,
+        0b1000'0000,
+        0b1000'0000,
+        0b1000'0000,
+        0b1111'0000,
+    },
+    Glyph{ // D
+        0b1110'0000,
+        0b1001'0000,
+        0b1001'0000,
+        0b1001'0000,
+        0b1110'0000,
+    },
+    Glyph{ // E
+        0b1111'0000,
+        0b1000'0000,
+        0b1111'0000,
+        0b1000'0000,
+        0b1111'0000,
+    },
+    Glyph{ // F
+        0b1111'0000,
+        0b1000'0000,
+        0b1111'0000,
+        0b1000'0000,
+        0b1000'0000,
+    },
+};
+
+auto load_font_into_memory(Memory& m, Address const a) -> bool;
+
+constexpr auto get_glyph_offset(Nibble const digit)
+    -> std::optional<std::size_t>
+{
+    if (digit >= font_glyphs.size()) {
+        return std::nullopt;
+    }
+
+    return glyph_size * digit;
+}
+
+}
+
+#endif
---
+++ libnpln/machine/Font.test.cpp
@@ -0,0 +1,83 @@
+// Copyright 2020 Jeremiah Griffin
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <libnpln/machine/Font.hpp>
+
+#include <catch2/catch.hpp>
+
+#include <iterator>
+#include <map>
+#include <type_traits>
+
+using namespace libnpln::machine;
+
+TEST_CASE("Glyphs store 5 bytes", "[machine][font]")
+{
+    static_assert(std::is_same_v<Glyph::value_type, Byte>);
+    REQUIRE(Glyph{}.max_size() == 5);
+    REQUIRE(Glyph{}.max_size() == glyph_size);
+}
+
+TEST_CASE("Font can be loaded into memory", "[machine][font]")
+{
+    Memory m;
+    constexpr Address base_addr = 0x100;
+    REQUIRE(load_font_into_memory(m, base_addr));
+
+    auto const base_iter = std::next(std::begin(m), base_addr);
+    for (std::size_t i = 0; i < font_glyphs.size(); ++i) {
+        auto const& g = font_glyphs[i];
+        REQUIRE(std::equal(
+            std::next(base_iter, g.size() * i),
+            std::next(base_iter, g.size() * (i + 1)),
+            std::begin(g), std::end(g)));
+    }
+}
+
+TEST_CASE("Font cannot be loaded past the end of memory", "[machine][font]")
+{
+    Memory m;
+    constexpr Address a = m.size() - glyph_size * font_glyphs.size() + 1;
+    REQUIRE_FALSE(load_font_into_memory(m, a));
+}
+
+TEST_CASE("Glyph offsets can be calculated", "[machine][font]")
+{
+    std::map<Nibble, std::size_t> const offsets = {
+        {0x0, 0},
+        {0x1, 5},
+        {0x2, 10},
+        {0x3, 15},
+        {0x4, 20},
+        {0x5, 25},
+        {0x6, 30},
+        {0x7, 35},
+        {0x8, 40},
+        {0x9, 45},
+        {0xA, 50},
+        {0xB, 55},
+        {0xC, 60},
+        {0xD, 65},
+        {0xE, 70},
+        {0xF, 75},
+    };
+    for (auto&& [g, o] : offsets) {
+        REQUIRE(get_glyph_offset(g) == o);
+    }
+}
+
+TEST_CASE("Unknown glyph offsets cannot be calculated", "[machine][font]")
+{
+    REQUIRE(get_glyph_offset(0x10) == std::nullopt);
+}
