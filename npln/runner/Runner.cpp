94576445948489eb5d6c1960fdc7eba7d4fe2d79
Jeremiah Griffin <jgriffin@mailbox.org>
2022-03-19 13:36:35 -0700

    Fix broken build on Apple

--- npln/runner/Runner.cpp
+++ npln/runner/Runner.cpp
@@ -68,7 +68,7 @@ auto Runner::create_window() -> void
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #endif
     window = glfwCreateWindow(1280, 720, "npln", nullptr, nullptr);
     if (window == nullptr) {
